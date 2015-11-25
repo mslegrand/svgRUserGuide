774f53bf21a773ef6edc28a8a3dc989114c723e9
sup <legrand@coanalytics.net>
2015-11-25 10:44:52 -0500

    posted on web

--- Util.R
+++ Util.R
@@ -228,11 +228,11 @@ toggleBar<-function(xy){
 
 #---------------------------------------------------------------
 #svgR logo generaor
-logo<-function(WH){
+logo<-function(WH, txt="Users Guide"){
   g(
     text( "svgR", cxy=WH*c(.25,.5), fill="darkblue", font.size=100, 
           font.family="san serif", stroke.width=3),
-    text( "Users Guide", cxy=WH*c(.7,.25), fill="darkblue", font.size=60, 
+    text( txt, cxy=WH*c(.7,.25), fill="darkblue", font.size=60, 
           font.family="fantasy", stroke.width=3),
     circle(cxy=WH*c(.25,.5), r=99, stroke.width=10, fill='none'  ),
     stroke='black',
@@ -257,6 +257,8 @@ logo<-function(WH){
     )
   )
 }
+
+
 
 logo2<-function(wh){
   g(
@@ -288,6 +290,49 @@ logo2<-function(wh){
     )
   )
 }
+
+
+#svgR logo generaor
+logo3<-function(WH=c(800,200), txt="Users Guide"){
+  r<-ceiling(.4*WH[2])
+  g(
+    text( "svgR", cxy=WH*c(.25,.5), fill="darkblue", font.size=r, 
+          font.family="san serif", stroke.width=3),
+    text( txt, cxy=WH*c(.7,.25), fill="darkblue", font.size=.6*r, 
+          font.family="fantasy", stroke.width=3),
+    circle(cxy=WH*c(.25,.5), r=r-1, stroke.width=10, fill='none'  ),
+    stroke='black',
+    filter = filter(
+      feMerge( 
+        feMergeNode(in1="SourceGraphic"),
+        feMergeNode(
+          in1=feComposite( operator='in',
+                           in1=feSpecularLighting( surfaceScale=6,
+                                                   specularConstant=1,
+                                                   specularExponent=30,
+                                                   lighting.color="yellow",
+                                                   in1=feGaussianBlur(
+                                                     stdDeviation=5,
+                                                     in1="SourceAlpha"),
+                                                   fePointLight(xyz=c(40,-30,200))
+                           ),
+                           in2="SourceAlpha"
+          )
+        )
+      )
+    )
+  )
+}
+
+
+WH3<-c(800,250)
+library(svgR)
+
+svgR( wh=WH3,
+logo3(WH=WH3, txt="Whirlwind Tutorial")
+)->tmp
+
+as.character(tmp,file="whirlwind.svg")
 
 #---------------------------------------------------------------
 #funkyFilter
